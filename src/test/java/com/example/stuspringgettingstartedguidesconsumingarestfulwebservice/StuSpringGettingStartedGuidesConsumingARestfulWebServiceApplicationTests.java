83cd2345cf2423e84ba788a92b7fd2eef764dbe9
cleanbrain.developer <cleanbrain.developer@gmail.com>
2023-11-11 20:03:57 +0900

    first commit

---
+++ src/test/java/com/example/stuspringgettingstartedguidesconsumingarestfulwebservice/StuSpringGettingStartedGuidesConsumingARestfulWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stuspringgettingstartedguidesconsumingarestfulwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StuSpringGettingStartedGuidesConsumingARestfulWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
